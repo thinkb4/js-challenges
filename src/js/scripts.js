a60c3f6edd95b174c2ac72c1c68d9450423fe684
Emilio Dominguez <emilioadominguezb@gmail.com>
2022-01-07 10:02:23 -0300

    Moved all projects to "challenges" folder

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,39 +1,40 @@
 const challenges = document.querySelector(".challenges");
+const projectsFolder = "./challenges";
 const projects = Object.freeze([
     {
         name: "Clock",
         thumbnail: "./src/assets/clock.jpg",
-        url: "./1 - Clock/index.html"
+        url: `${projectsFolder}/1 - Clock/index.html`
     },
     {
         name: "Rock, Paper, Scissors, Lizard, Spock",
         thumbnail: "./src/assets/rock-paper-scissors-lizard-spock.png",
-        url: "./2 - Rock, Paper, Scissors, Lizard, Spock/index.html"
+        url: `${projectsFolder}/2 - Rock, Paper, Scissors, Lizard, Spock/index.html`
     },
     {
         name: "Photo Filters",
         thumbnail: "./src/assets/photo-filters.jpg",
-        url: "./3 - Photo Filters/index.html"
+        url: `${projectsFolder}/3 - Photo Filters/index.html`
     },
     {
         name: "Pixel Art",
         thumbnail: "./src/assets/pixel-art.png",
-        url: "./4 - Pixel Art/index.html"
+        url: `${projectsFolder}/4 - Pixel Art/index.html`
     },
     {
         name: "Slider",
         thumbnail: "./src/assets/slider.png",
-        url: "./5 - Slider/index.html"
+        url: `${projectsFolder}/5 - Slider/index.html`
     },
     {
         name: "Puzzle",
         thumbnail: "./src/assets/puzzle.png",
-        url: "./6 - Puzzle/index.html"
+        url: `${projectsFolder}/6 - Puzzle/index.html`
     },
     {
         name: "Weather",
         thumbnail: "./src/assets/weather.png",
-        url: "./7 - Weather/index.html"
+        url: `${projectsFolder}/7 - Weather/index.html`
     }
 ]);
 
